9c91080335e7d066facf968d9f67d8004715fcaf
Pavan Ade <126013713+pavan-ade@users.noreply.github.com>
2025-03-10 11:59:12 +0530

    2project set-up

---
+++ demo-spring/src/main/java/com/test/App.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println("Spring application started successfully!");
+    }
+}
